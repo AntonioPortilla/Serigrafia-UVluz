cc7e236075535748d6f0a6159b820594cb7e33f5
JALP <julioantonio.lp@gmail.com>
2022-05-05 16:21:09 -0500

    tpl3

--- uv-defines.php
+++ uv-defines.php
@@ -2,10 +2,10 @@
 	define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/');
 	//define('URL_PRIVATE', ROOT_PATH.'/dante/Serigrafia-UVluz');	
 	//define('APP_DOMAIN', 'http://localhost/dante/Serigrafia-UVluz');
-echo '<pre>';
-echo ROOT_PATH;
-echo '</pre>';
-	define('URL_PRIVATE', 'http://agenciamayor.com/portfolio/Serigrafia-UVluz');	
+	//echo '<pre>';
+	//echo ROOT_PATH;
+	//echo '</pre>';
+	define('URL_PRIVATE', ROOT_PATH.'/portfolio/Serigrafia-UVluz');	
 	define('APP_DOMAIN', 'http://agenciamayor.com/portfolio/Serigrafia-UVluz');
 	define('TPL_PATH', URL_PRIVATE.'/public/tpl');
 	//define('PUBLIC_PATH', 'http://localhost/dante/Serigrafia-UVluz');
